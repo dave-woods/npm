8cbae88f08bee5b42a45a7c66bb85e8376644c85
Dave Woods <dwoodscs@gmail.com>
2016-05-08 14:36:19 +0000

    Completed lesson 13 (JSON requests)

---
+++ learnyounode/13/program.js
@@ -0,0 +1,104 @@
+/*
+  Write an HTTP server that serves JSON data when it receives a GET request  
+  to the path '/api/parsetime'. Expect the request to contain a query string  
+  with a key 'iso' and an ISO-format time as the value.  
+   
+  For example:  
+   
+  /api/parsetime?iso=2013-08-10T12:10:15.474Z  
+   
+  The JSON response should contain only 'hour', 'minute' and 'second'  
+  properties. For example:  
+   
+     {  
+       "hour": 14,  
+       "minute": 23,  
+       "second": 15  
+     }  
+   
+  Add second endpoint for the path '/api/unixtime' which accepts the same  
+  query string but returns UNIX epoch time in milliseconds (the number of  
+  milliseconds since 1 Jan 1970 00:00:00 UTC) under the property 'unixtime'.  
+  For example:  
+   
+     { "unixtime": 1376136615474 }  
+   
+  Your server should listen on the port provided by the first argument to  
+  your program.
+*/
+
+var http = require("http");
+var port = Number(process.argv[2]);
+var url = require("url");
+
+http.createServer(function(req, res){
+    if (req.method != "GET")
+    {
+        res.writeHead(404);
+        return res.end("GET request needed, but not received");
+    }
+        
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var urlparams = url.parse(req.url, true);
+    var date = new Date(urlparams.query.iso);
+    if (urlparams.pathname == "/api/parsetime")
+    {
+        var obj = {
+            "hour":date.getHours(),
+            "minute":date.getMinutes(),
+            "second":date.getSeconds()
+        };
+        return res.end(JSON.stringify(obj));
+    }
+    else if (urlparams.pathname == "/api/unixtime")
+    {
+        var obj = {
+            "unixtime":date.getTime()
+        };
+        return res.end(JSON.stringify(obj));
+    }
+    else
+    {
+        res.writeHead(404);
+        return res.end("Unrecognised format requested: " + urlparams.pathname);
+    }
+}).listen(port);
+
+// official solution
+
+/*
+     var http = require('http')  
+     var url = require('url')  
+       
+     function parsetime (time) {  
+       return {  
+         hour: time.getHours(),  
+         minute: time.getMinutes(),  
+         second: time.getSeconds()  
+       }  
+     }  
+       
+     function unixtime (time) {  
+       return { unixtime : time.getTime() }  
+     }  
+       
+     var server = http.createServer(function (req, res) {  
+       var parsedUrl = url.parse(req.url, true)  
+       var time = new Date(parsedUrl.query.iso)  
+       var result  
+       
+       if (/^\/api\/parsetime/.test(req.url))  
+         result = parsetime(time)  
+       else if (/^\/api\/unixtime/.test(req.url))  
+         result = unixtime(time)  
+       
+       if (result) {  
+         res.writeHead(200, { 'Content-Type': 'application/json' })  
+         res.end(JSON.stringify(result))  
+       } else {  
+         res.writeHead(404)  
+         res.end()  
+       }  
+     })  
+     server.listen(Number(process.argv[2]))
+*/
