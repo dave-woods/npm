3111944f334abff54850aee039d9692399f39e95
Dave Woods <dwoodscs@gmail.com>
2016-05-08 17:41:16 +0000

    Completed lesson 7 (query params)

---
+++ expressjs/7/program.js
@@ -0,0 +1,12 @@
+/*
+Write a route that extracts data from query string in the GET '/search' URL
+route, e.g. ?results=recent&include_tabs=true and then outputs it back to
+the user in JSON format.
+*/
+
+var express = require("express");
+var app = express();
+app.get("/search", function(req, res){
+    res.send(req.query);
+});
+app.listen(Number(process.argv[2]));
